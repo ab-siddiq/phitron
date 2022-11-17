07559e84ace2d2cc1098e82a2e7fe2bf5e00a063
siddiq <siddiq.aiub@gmail.com>
2022-11-17 09:18:29 +0600

    one iteration modified

--- phitron-c/practice-problem/square_pattern.c
+++ phitron-c/practice-problem/square_pattern.c
@@ -3,7 +3,7 @@ int main(){
     int n;
     scanf("%d",&n);
     for(int i=0;i<n;i++){
-        for(int k=n;k>=i;k--){
+        for(int k=n;k>i;k--){
             printf(" ");
         }
         for(int j=0;j<n;j++){
