60089a98f84b47d5f483260280c08d1b2f1aebb1
siddiq <siddiq.aiub@gmail.com>
2023-01-08 16:45:41 +0600

    twin number

---
+++ phitron-c/lab-final/twin_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n,s;
+    scanf("%d%d",&n,&s);
+    printf("%d",(s-n)/2);
+    return 0;
+}
